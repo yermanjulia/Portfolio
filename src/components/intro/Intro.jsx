9219ef6ea2f891a811e051621cb5b5e2c0200555
Julia Yerman <juliyaerm88@gmail.com>
2023-05-31 22:35:59 -0700

    Crafted introduction page

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,5 +1,20 @@
 import "./intro.scss";
 
 export default function Intro() {
-  return <div className="intro" id="intro"></div>;
+  return (
+    <div className="intro" id="intro">
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/logo.jpeg" alt="" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h3>Hey, I Am</h3>
+          <h1>JULIA</h1>
+          <h4>Projects | Resume | Blog</h4>
+        </div>
+      </div>
+    </div>
+  );
 }
