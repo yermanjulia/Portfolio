f7a355a1076a0c10c934b36d4b0b2852eff6f1e9
Julia Yerman <juliyaerm88@gmail.com>
2023-06-06 14:22:35 -0700

    Adding Dynamic Active Class

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,21 +1,47 @@
+import { useEffect, useState } from "react";
+import PortfolioList from "../portfolioList/PortfolioList";
 import "./portfolio.scss";
 
 export default function Portfolio() {
+  const [selected, setSelected] = useState("project1");
+  const list = [
+    {
+      id: "project1",
+      title: "Project 1",
+    },
+    {
+      id: "project2",
+      title: "Project 2",
+    },
+    { id: "project3", title: "Project 3" },
+  ];
+
   return (
     <div className="portfolio" id="portfolio">
-      <h1>Portfolio</h1>
+      <h1>PORTFOLIO</h1>
       <ul>
-        <li className="active">Project 1</li>
-        <li>Project 2</li>
+        {list.map((item) => (
+          <PortfolioList
+            title={item.title}
+            active={selected === item.id}
+            setSelected={setSelected}
+            id={item.id}
+          />
+        ))}
       </ul>
+
       <div className="container">
         <div className="item">
-          <img src="assets/PlayStation-logo.png" alt=""></img>
-          <h3> PlayStation Project</h3>
+          <img src="./assets/mockup.png" alt="" />
+          <h3>PlayStation</h3>
         </div>
         <div className="item">
-          <img src="assets/logo_nucamp.png" alt=""></img>
-          <h3> Nucamp Project</h3>
+          <img src="./assets/mockup.png" alt="" />
+          <h3>Nucamp</h3>
+        </div>
+        <div className="item">
+          <img src="./assets/mockup.png" alt="" />
+          <h3>WeatherApp</h3>
         </div>
       </div>
     </div>
---
+++ src/data.js
@@ -0,0 +1,14 @@
+export const project1Portfolio = [
+  {
+    id: 1,
+    title: "PlayStation",
+  },
+  {
+    id: 2,
+    title: "Nucamp",
+  },
+  {
+    id: 3,
+    title: "Weather",
+  },
+];
