6d8f29b578907b6433b414247084880ab686b49b
Julia Yerman <juliyaerm88@gmail.com>
2023-06-09 12:33:26 -0700

    WIP

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -11,17 +11,20 @@ export default function About() {
       <div className="right">
         <h1>ABOUT </h1>
         <div className="wrapper">
-          <h4>
+          <p>
             I'm a Frontend Developer & QA Engineer with years of experience in
             tech. Specializing in frontend development, I use HTML5, CSS,
             JavaScript, Bootstrap, React, and React Native to create responsive
-            web and mobile applications. As a strong proponent for women in
-            tech, I actively support initiatives empowering women in STEM fields
-            and enjoy fostering a more diverse, inclusive tech industry. I'm
-            eager to explore new technologies, continue learning, and
-            collaborate to create solutions that make a positive impact.
-          </h4>
-          <h3> </h3>
+            web and mobile applications.
+          </p>
+          <br />
+          <p>
+            As a strong proponent for women in tech, I actively support
+            initiatives empowering women in STEM fields and enjoy fostering a
+            more diverse, inclusive tech industry. I'm eager to explore new
+            technologies, continue learning, and collaborate to create solutions
+            that make a positive impact.
+          </p>
         </div>
       </div>
     </div>
